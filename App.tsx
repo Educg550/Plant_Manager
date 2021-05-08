37f9c42510451bea8713dbc770f69f44d8ec908e
Educg550 <eduardo9cruz@outlook.com>
2021-05-08 12:30:23 -0300

    Welcome, User Input and Confirmation screens finished
    Routes declared for navigation between these

--- App.tsx
+++ App.tsx
@@ -1,19 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Welcome } from './src/pages/Welcome';
+import AppLoading from 'expo-app-loading'
+
+import Routes from './src/routes';
+
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold
+} from '@expo-google-fonts/jost'
 
 export default function App() {
+  const [ isFontLoaded ] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold
+  })
+
+  // Tela de splash enquanto as fontes não estiverem carregadas
+  if (!isFontLoaded) {
+    return <AppLoading />
+  }
+
   return (
-    <Welcome />
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#313131',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
